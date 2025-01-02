7b5ee3f89ba4a88e015b5c355b3ba1536dacf4d0
zrz <2334160007@qq.com>
2025-01-02 11:38:27 +0800

    添加了基于提示工程的简单分类、简单的AI广告词设计

---
+++ vue/src/components/Search.vue
@@ -0,0 +1,34 @@
+<template>
+  <!--      搜索栏-->
+  <el-row>
+    <el-col :span="8">
+      <a href="/topview">
+        <img src="../resource/img/logo.png" style="float: right;width: 100px;height: 100px"/>
+      </a>
+    </el-col>
+    <el-col :span="16">
+      <div class="search" style="display: inline-block">
+        <div style="height: 60px;margin-left: 30px">
+          <input @keydown.enter="$emit('search',searchText)" type="text" placeholder="请输入要查找的商品" v-model="searchText">
+          <button @click="$emit('search',searchText)"></button>
+        </div>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data(){
+    return{
+      searchText:'',
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
